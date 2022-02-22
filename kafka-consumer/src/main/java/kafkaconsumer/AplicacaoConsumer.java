e08ab5d75e81ec6e4158e91216a3e7f5324911fb
Leonardo <66699109+Leonardohilariogithub@users.noreply.github.com>
2022-02-22 15:35:11 -0300

    kafka

---
+++ kafka-consumer/src/main/java/kafkaconsumer/AplicacaoConsumer.java
@@ -0,0 +1,20 @@
+package kafkaconsumer;
+
+
+import kafkaconsumer_eventos.ConsumidorEvento;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class AplicacaoConsumer {
+
+    public static void main(String[] args) {
+        AplicacaoConsumer aplicacao = new AplicacaoConsumer();
+        aplicacao.iniciar();
+    }
+
+    private void iniciar() {
+        log.info("Iniciando a aplicação");
+        ConsumidorEvento consumidor = new ConsumidorEvento();
+        consumidor.executar();
+    }
+}
